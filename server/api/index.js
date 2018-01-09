61956c0f055d443dccc0e3e301ffcb80402b0790
Will Shaw <wrcs505@gmail.com>
2018-01-09 16:53:12 -0500

    models index routing in progress

--- server/api/index.js
+++ server/api/index.js
@@ -2,6 +2,12 @@ const router = require('express').Router()
 module.exports = router
 
 router.use('/users', require('./users'))
+router.use('/orders', require('./orders'))
+router.use('/products', require('./products'))
+router.use('/reviews', require('./reviews'))
+router.use('/categories', require('./categories'))
+router.use('/admin', require('./admin'))
+
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
