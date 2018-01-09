375161f6a26a7c1b4eb29cc4c7e8451bbe528463
elanamig <{elanamig}@users.noreply.github.com>
2018-01-09 18:13:36 -0500

    seed file

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,4 +1,9 @@
-const User = require('./user')
+const User = require('./user');
+const Product = require ('./product');
+const Order = require ('./order');
+const Category = require ('./category');
+const Review = require ('./review');
+
 
 /**
  * If we had any associations to make, this would be a great place to put them!
@@ -13,6 +18,25 @@ const User = require('./user')
  * for example, we can say: const {User} = require('../db/models')
  * instead of: const User = require('../db/models/user')
  */
+
+ //will add getCategories/setCategories/addCategories/addCategory to Product
+Product.belongsToMany(Category, {through: 'CategoryProduct'});
+//will add getProducts/setProducts/addProducts/addProduct to Category
+Category.belongsToMany (Product, {through: 'CategoryProduct'});
+
+
+//adds userId, getUser, setUer to Order
+Order.belongsTo(User);  
+//adds userId to Order, getOrders/setOrders to User
+User.hasMany(Order);
+
+//Review gets UserId
+Review.belongsTo(User);
+//One to many, User gets getReviews and setReviews, reviews gets userId
+User.hasMany(Review);
+//adds productId to Review, and set/get Reviews to Product
+Product.hasMany(Review);
+
 module.exports = {
-  User
+  User, Category, Product, Review, Order
 }
--- server/db/models/order.js
+++ server/db/models/order.js
@@ -17,7 +17,7 @@ const Order = db.define('order', {
   subTotal: {
     type: Sequelize.VIRTUAL,
     get: function() {
-      if (this.items.length) {
+      if (this.items && this.items.length) {
         return this.items.map(item => item.quantity * item.price);
       } else {
         return 0;
@@ -27,8 +27,8 @@ const Order = db.define('order', {
   total: {
     type: Sequelize.VIRTUAL,
     get: function() {
-      if (subTotal) {
-        return subTotal * 1.08875;
+      if (this.subTotal) {
+        return this.subTotal * 1.08875;
       } else {
         return 0;
       }
@@ -36,6 +36,12 @@ const Order = db.define('order', {
   },
   recipientName: {
     type: Sequelize.STRING
+  },
+  recipientEmail: {
+    type: Sequelize.STRING,
+    validate:{
+        isEmail : true
+    }
   },
   recipientAddress: {
     type: Sequelize.STRING
@@ -56,27 +62,34 @@ Order.countByCategoryId = function (id) {
   });
 };
 
-Order.afterCreate( (instance, options) => {
-  const items = instance.items;
-  //pseudocode
-  //1. convert json to obj
-  //2. get the product id
-  //3. look up categories for that product
-  //4. for each category, inc the rank counter
-  items.forEach (item => {
-    const obj = JSON.parse(item);
-    const prodId = obj.productId;
-    const prod = Product.findById(prodId)
-    .then (product => {
-      //assume an array
-      product.getCategories().forEach(category => {
-        //Might have to do promise.all
-        category.incrementRank();
-      });
-    });
+// Order.afterCreate( (instance, options) => {
+//   console.log("Order.afterCreate");
+//   const items = instance.items;
+//   //pseudocode
+//   //1. convert json to obj
+//   //2. get the product id
+//   //3. look up categories for that product
+//   //4. for each category, inc the rank counter
+//   items.forEach (item => {
+//     const prodId = item.productId;
+//     console.log("one", prodId);
+//     const prod = Product.findById(prodId)
+//     .then (product => {
+//       //assume an array
+//       console.log("2", product);
+//       product.getCategories()
+//       .then ( categories => {
+//         console.log("got categories", categories);
+//         categories.forEach(category => {
+//         //Might have to do promise.all
+//         console.log("order.js, about to increment rank");
+//           category.incrementRank();
+//         });
+//       });
+//     });
 
-  });
-});
+//   });
+// });
 
 module.exports = Order;
 
