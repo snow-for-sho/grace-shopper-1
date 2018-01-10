e9d5a327bf0fa6a54673905c2794e02df00b63c2
aubreyarcangel <aarcangel@gmail.com>
2018-01-10 13:09:44 -0500

    created orders api

--- server/api/index.js
+++ server/api/index.js
@@ -4,9 +4,9 @@ module.exports = router
 router.use('/users', require('./users'))
 router.use('/orders', require('./orders'))
 router.use('/products', require('./products'))
-router.use('/reviews', require('./reviews'))
-router.use('/categories', require('./categories'))
-router.use('/admin', require('./admin'))
+// router.use('/reviews', require('./reviews'))
+// router.use('/categories', require('./categories'))
+// router.use('/admin', require('./admin'))
 
 
 router.use((req, res, next) => {
